89eae199fa1aab9f3e0a0e51540bdc0927ee9458
Richkazz <edawarekaro@gmail.com>
2023-09-13 10:51:29 +0100

    person not found error message

--- HNGx/Constants/ErrorConstants.cs
+++ HNGx/Constants/ErrorConstants.cs
@@ -3,7 +3,7 @@
     public static class ErrorConstants
     {
         public const string PERSON_NAME_ERROR = "Person name is required.";
-        public const string PERSON_ID_ERROR = "ID not valid";
-        public const string PERSON_NOT_FOUND_ERROR = "ID not valid";
+        public const string PERSON_ID_ERROR = "ID not valid.";
+        public const string PERSON_NOT_FOUND_ERROR = "Person not found.";
     }
 }
